dbe21f2062fe0f6406182d8ab0780541e8b193e5
lucasalisboa <43450181+lucasalisboa@users.noreply.github.com>
2019-05-22 21:05:45 -0300

    Adding Undo/Redo

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
     {
 
         Locale.setDefault(Locale.US);
-	    System.out.println("CHOOSE YOUR OPERATION:");
+        System.out.println("CHOOSE YOUR OPERATION:");
         System.out.println("1- ADD NEW EMPLOYER");
         System.out.println("2- REMOVE A EMPLOYER");
         System.out.println("3- PUT THE POINT CARD");
@@ -17,56 +17,61 @@ public class Main {
         System.out.println("5- PUT THE SERVICE TAX");
         System.out.println("6- CHANGE THE EMPLOYER INFORMATIONS");
         System.out.println("7- RUN THE PAYROLL");
-        System.out.println("8- SHOW THE PAYMENT SCHEDULE");
-        System.out.println("9- CREATE A NEW PAYMENT SCHEDULE");
+        System.out.println("8- UNDO/REDO");
+        System.out.println("9- SHOW THE PAYMENT SCHEDULE");
+        System.out.println("10- CREATE A NEW PAYMENT SCHEDULE");
 
         int operation;
-	    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         operation = sc.nextInt();
         System.out.println(" ");
 
-	if(operation == 1 )
-    {
+        if(operation == 1 )
+        {
 
-    }
-	else if(operation == 2 )
-    {
+        }
+        else if(operation == 2 )
+        {
 
-    }
-    else if(operation == 3 )
-    {
+        }
+        else if(operation == 3 )
+        {
 
-    }
-    else if(operation == 4 )
-    {
+        }
+        else if(operation == 4 )
+        {
 
-    }
-    else if(operation == 5 )
-    {
+        }
+        else if(operation == 5 )
+        {
 
-    }
-    else if(operation == 6 )
-    {
+        }
+        else if(operation == 6 )
+        {
 
-    }
-    else if(operation == 7 )
-    {
+        }
+        else if(operation == 7 )
+        {
 
-    }
-    else if(operation == 8 )
-    {
+        }
+        else if(operation == 8 )
+        {
 
-    }
-    else if(operation == 9 )
-    {
+        }
+        else if(operation == 9 )
+        {
 
-    }
-    else
-    {
-        System.out.println("INVALID OPERATION");
-        System.out.println("PLEASE, TRY AGAIN\n");
-        main(args);
-    }
+        }
+        else if(operation == 10 )
+        {
+
+        }
+        else
+        {
+            System.out.println("INVALID OPERATION");
+            System.out.println("PLEASE, TRY AGAIN\n");
+            main(args);
+        }
 
     }
 }
