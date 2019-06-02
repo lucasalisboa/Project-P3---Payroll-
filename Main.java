a330f0c36b0112993083a905dc72e5b0058facef
lucasalisboa <43450181+lucasalisboa@users.noreply.github.com>
2019-06-02 13:41:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,22 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("WELCOME!\n");
-        String[][] payroll  = new String[100][10];
-        int[][] calendar  = new int [12][31];
+        String[][] payroll = new String[100][10];
+        int[][] calendar = new int[12][31];
         int c1 = 0, c2 = 0;
 
-        while(c1<100)
-        {
-            if(c2 == 10)
-            {
+        while (c1 < 100) {
+            if (c2 == 10) {
                 c1++;
                 c2 = 0;
-            }
-            else
-            {
+            } else {
                 payroll[c1][c2] = "NULL";
                 c2++;
             }
@@ -28,48 +23,33 @@ public class Main {
         c1 = 0;
         c2 = 0;
 
-        while(c1 < 12)
-        {
-            if(c1 % 2 == 0)
-            {
-                if(c2 == 30)
-                {
+        while (c1 < 12) {
+            if (c1 % 2 == 0) {
+                if (c2 == 30) {
                     calendar[c1][c2] = 1;
                     c1++;
                     c2 = 0;
-                }
-                else
-                {
+                } else {
                     calendar[c1][c2] = 1;
                     c2++;
                 }
-            }
-            else if(c1 == 1)
-            {
-                if(c2 == 27)
-                {
+            } else if (c1 == 1) {
+                if (c2 == 27) {
                     calendar[c1][c2] = 1;
-                    calendar[c1][c2+1] = 0;
+                    calendar[c1][c2 + 1] = 0;
                     c1++;
                     c2 = 0;
-                }
-                else
-                {
+                } else {
                     calendar[c1][c2] = 1;
                     c2++;
                 }
-            }
-            else
-            {
-                if(c2 == 29)
-                {
+            } else {
+                if (c2 == 29) {
                     calendar[c1][c2] = 1;
-                    calendar[c1][c2+1] = 0;
+                    calendar[c1][c2 + 1] = 0;
                     c1++;
                     c2 = 0;
-                }
-                else
-                {
+                } else {
                     calendar[c1][c2] = 1;
                     c2++;
                 }
@@ -81,13 +61,13 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         c2 = sc.nextInt();
-        c2 --;
+        c2--;
 
         System.out.println("MONTH:");
         c1 = sc.nextInt();
-        c1 --;
+        c1--;
 
-        System.out.printf("YOUR DATA IS %d/%d\n",c2+1,c1+1);
+        System.out.printf("YOUR DATA IS %d/%d\n", c2 + 1, c1 + 1);
 
         System.out.println("THIS DATA IS:");
         System.out.println("1- MONDAY");
@@ -103,12 +83,11 @@ public class Main {
 
         System.out.println("\n");
 
-        action(calendar,c1,c2,day,payroll);
+        action(calendar, c1, c2, day, payroll);
         sc.close();
     }
 
-    public static void action(int calendar[][],int c1,int c2,int day, String payroll[][])
-    {
+    public static void action(int calendar[][], int c1, int c2, int day, String payroll[][]) {
 
         Locale.setDefault(Locale.US);
         System.out.println("CHOOSE YOUR OPERATION:");
@@ -131,91 +110,63 @@ public class Main {
         System.out.println(" ");
 
 
-        if(operation == 1 )
-        {
-            hire(payroll,0);
-            action(calendar,c1,c2,day,payroll);
-        }
-        else if(operation == 2 )
-        {
+        if (operation == 1) {
+            hire(payroll, 0);
+            action(calendar, c1, c2, day, payroll);
+        } else if (operation == 2) {
             String name;
             sc.nextLine();
             name = sc.nextLine();
-            fire(name,payroll,0);
-            action(calendar,c1,c2,day,payroll);
-        }
-        else if(operation == 3 )
-        {
+            fire(name, payroll, 0);
+            action(calendar, c1, c2, day, payroll);
+        } else if (operation == 3) {
+            String name;
+            sc.nextLine();
+            name = sc.nextLine();
+            point(name, payroll, 0);
+            action(calendar, c1, c2, day, payroll);
+        } else if (operation == 4) {
 
-        }
-        else if(operation == 4 )
-        {
+        } else if (operation == 5) {
 
-        }
-        else if(operation == 5 )
-        {
+        } else if (operation == 6) {
 
-        }
-        else if(operation == 6 )
-        {
+        } else if (operation == 7) {
 
-        }
-        else if(operation == 7 )
-        {
+        } else if (operation == 8) {
 
-        }
-        else if(operation == 8 )
-        {
+        } else if (operation == 9) {
 
-        }
-        else if(operation == 9 )
-        {
+        } else if (operation == 10) {
 
-        }
-        else if(operation == 10 )
-        {
-
-        }
-        else if(operation == 11)
-        {
-            if(c2 == 30 || calendar[c1][c2+1] == 0)
-            {
+        } else if (operation == 11) {
+            if (c2 == 30 || calendar[c1][c2 + 1] == 0) {
                 c2 = 0;
 
-                if(c1 == 11)
-                {
+                if (c1 == 11) {
                     c1 = 0;
-                }
-                else
-                {
+                } else {
                     c1++;
                 }
 
-            }
-            else
-            {
+            } else {
                 c2++;
             }
             day++;
-            if(day == 8)
-            {
+            if (day == 8) {
                 day = 1;
             }
-            action(calendar,c1,c2,day,payroll);
-        }
-        else
-        {
+            action(calendar, c1, c2, day, payroll);
+        } else {
             System.out.println("INVALID OPERATION");
             System.out.println("PLEASE, TRY AGAIN\n");
-            action(calendar,c1,c2,day,payroll);
+            action(calendar, c1, c2, day, payroll);
         }
 
     }
 
-    public static void hire(String payroll[][],int c1)
-    {
-        if(payroll[c1][0] == "NULL")
-        {
+    public static void hire(String payroll[][], int c1) {
+        if (payroll[c1][0] == "NULL") {
             Scanner sc = new Scanner(System.in);
 
             System.out.println("WHAT'S THE NAME?");
@@ -234,13 +185,10 @@ public class Main {
             System.out.println("SYNDICATED?");
             payroll[c1][4] = sc.nextLine();
 
-            if(payroll[c1][4] == "NO")
-            {
+            if (payroll[c1][4].equals("NO")) {
                 payroll[c1][5] = "0";
                 payroll[c1][6] = "NULL";
-            }
-            else
-            {
+            } else {
                 System.out.println("WHAT'S THE TAX?");
                 payroll[c1][5] = sc.nextLine();
                 System.out.println("WHAT'S THE IDENTIFICATION?");
@@ -253,29 +201,46 @@ public class Main {
             System.out.println("BA- BANK ACCOUNT; CH- CHECK IN HANDS; CC- CHECK BY COURIER");
             payroll[c1][8] = sc.nextLine();
 
-        }
-        else
-        {
+        } else {
             c1++;
-            hire(payroll,c1);
+            hire(payroll, c1);
         }
     }
-    public static void fire(String name,String payroll[][],int c1)
-    {
-        if(payroll[c1][0] == name)
-        {
-            int c2=0;
-            while(c2 < 10)
-            {
+
+    public static void fire(String name, String payroll[][], int c1) {
+        if (payroll[c1][0].equals(name)) {
+            int c2 = 0;
+            while (c2 < 10) {
                 payroll[c1][c2] = "NULL";
                 c2++;
             }
-            System.out.printf("EMPLOYER %s was remover\n",name);
-        }
-        else
-        {
+            System.out.printf("Employer %s was remover\n\n", name);
+        } else if (c1 == 100) {
+            System.out.println("Employer doesn't exist\n");
+        } else {
             c1++;
-            fire(name,payroll,c1);
+            fire(name, payroll, c1);
+        }
+    }
+
+    public static void point(String name, String payroll[][], int c1) {
+        if (payroll[c1][0].equals(name))
+        {
+            System.out.println("HOW MANY HOURS THE EMPLOYER WORKED TODAY?");
+            int hours = Integer.parseInt(payroll[c1][7]);
+            Scanner sc = new Scanner(System.in);
+            int h = sc.nextInt();
+            int nh = hours + h;
+            payroll[c1][7] = Integer.toString(nh);
+            System.out.printf("The employer has %s hours worked\n",payroll[c1][7]);
+        }
+        else if (c1 == 100)
+        {
+            System.out.println("Employer doesn't exist\n");
+        } else
+            {
+            c1++;
+            point(name, payroll, c1);
         }
     }
 }
